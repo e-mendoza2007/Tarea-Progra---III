cf09abdf3f2997ca8877d926e75bbdefd368ef09
e-mendoza2007 <ernesto.mendoza@utec.edu.pe>
2026-04-02 09:57:16 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Tensor {
+
+    public:
+    Tensor ( const  vector < size_t >& shape , const   vector <double >& values ) ;
+};
